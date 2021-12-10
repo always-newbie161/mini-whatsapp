c0f6abdcccb9e6e8b93f991fe722c94d6c7ffde9
Srikar Jilugu <66471669+always-newbie161@users.noreply.github.com>
2021-12-11 00:45:38 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import os
 import glob
+import tqdm, time
 from cryptography.fernet import Fernet
 
 PORT = 9999
@@ -70,60 +71,79 @@ def handle_client(name, conn, addr):
                 print("Filesize:", filesize, "bytes")
 
                 send_client(name, "[SERVER GRANT] UPLOAD")
-                filename_loc = "server/" + filename 
-                
+                filename_loc = "server/" + filename
+
                 NUM_CHUNKS = int(filesize) // BUF_SIZE + 1
-                with open(filename_loc,"wb") as file:
+                packet_size = conn.recv(BUF_SIZE).decode()
+                progress = tqdm.tqdm(range(int(filesize)), desc=f"Recieving {filename}", unit="B", unit_scale=True,
+                                     unit_divisor=1024)
+                with open(filename_loc, "wb") as file:
                     for _ in range(NUM_CHUNKS):
-                        chunk = conn.recv(BUF_SIZE)
+                        chunk = conn.recv(int(packet_size)).decode()
                         if not chunk:
                             break
-                        file.write(chunk)
+                        key, encr_msg = chunk.split('#')
+                        f = Fernet(key.encode())
+                        decr_chunk = f.decrypt(encr_msg.encode())
+                        file.write(decr_chunk)
+                        # Update the progress bar
+                        progress.update(len(decr_chunk))
+                    progress.close()
 
                 print(f"File {filename} Received from {name}")
+                send_client(name, f"[SERVER UPDATE] Hi {name}, your file {filename} has been successfully uploaded")
                 send_all(name, f"[SERVER UPDATE] {name} has uploaded file {filename} to the server")
 
             elif msg.startswith(DOWNLOAD_MSG):
                 _, filename = msg.split(' ')
-                filename = "server/" + filename
-                print(f'[CLIENT REQUEST] DOWNLOAD {filename}')
-                if os.path.exists(filename):
+                filename_loc = "server/" + filename
+                print(f'[CLIENT REQUEST] DOWNLOAD {filename} from {addr[1]}, Name: {name}')
+                if os.path.exists(filename_loc):
                     print(filename + " found")
-                filesize = os.path.getsize(filename)
+                filesize = os.path.getsize(filename_loc)
 
                 send_client(name, f"[SERVER GRANT] DOWNLOAD {filesize} bytes")
 
                 NUM_CHUNKS = int(filesize) // BUF_SIZE + 1
-                with open(filename_loc,"rb") as file:
-                    for _ in range(NUM_CHUNKS):
+                progress = tqdm.tqdm(range(int(filesize)), desc=f"Sending {filename}", unit="B", unit_scale=True,
+                                     unit_divisor=1024)
+                with open(filename_loc, "rb") as file:
+                    for i in range(NUM_CHUNKS):
                         chunk = file.read(BUF_SIZE)
                         if not chunk:
                             break
-                        send_allbytes(conn, chunk)
-            print(f"File {filename} Downloaded to client {name}")
-
+                        key = gen_key()
+                        f = Fernet(key)
+                        encr_packet = '#'.join([key.decode(), f.encrypt(chunk).decode()]).encode()
+                        if i == 0:
+                            packet_size = len(encr_packet)
+                            conn.send(str(packet_size).encode())
+                            time.sleep(0.5)
+                        send_allbytes(conn, encr_packet)
+                        # Update the progress bar
+                        progress.update(len(chunk))
+                    progress.close()
+                print(f"File {filename} Downloaded to client {name}")
 
             elif msg.startswith(LIST_MSG):
                 tokens = msg.split(' ')
                 if len(tokens) == 1:
                     encoded_list = '\n'.join(glob.glob(r'server/*', recursive=True))
-                    print('[CLIENT REQUEST] LIST ALL FILES')
+                    print(f'[CLIENT REQUEST] LIST ALL FILES  from {addr[1]}, Name: {name}')
                 else:
                     re_pattern = tokens[1]
                     encoded_list = '\n'.join(glob.glob(r'server/' + re_pattern, recursive=True))
-                    print(f'[CLIENT REQUEST] LIST {re_pattern} FILES')
+                    print(f'[CLIENT REQUEST] LIST {re_pattern} FILES  from {addr[1]}, Name: {name}')
 
                 encoded_list = encoded_list if len(encoded_list) else 'No file found :('
                 send_client(name, f"[SERVER GRANT] TO LIST all the requested files:\n{encoded_list}")
 
             else:
-                key, encr_msg = msg.split('#')
-                f = Fernet(key)
-                decr_msg = f.decrypt(encr_msg.encode()).decode()
-                print(f"{decr_msg}")
                 send_all(name, msg)
-        else:
-            print(f"reading msg even without user input!")
+    else:
+        print(f"reading msg even without user input!")
+
+
     conn.close()
 
 
